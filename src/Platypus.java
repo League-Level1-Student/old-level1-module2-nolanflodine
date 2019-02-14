13b6e09f4b6c0637b0d120f7d3e647411b23436b
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-02-13 16:58:05 -0800

    platypus

--- src/Platypus.java
+++ src/Platypus.java
@@ -1,7 +1,10 @@
 
 public class Platypus {
       private String name;
-
+Platypus(String name){
+	this.name=name;
+	System.out.println("constructor");
+}
        void sayHi(){
              System.out.println("The platypus " + name + " is smarter than your average platypus.");
        }
--- src/PlatypusRunner.java
+++ src/PlatypusRunner.java
@@ -1,6 +1,9 @@
 
 public class PlatypusRunner {
 public static void main(String[] args) {
-	
+	Platypus p=new Platypus("Perry");
+	Platypus l=new Platypus("Larry");
+	p.sayHi();
+	l.sayHi();
 }
 }
